ba0137bce5310e4d9219ccece8a59e79b2c5dce2
Isi <isinsemail@gmail.com>
2024-09-18 19:07:40 +0100

    Update image_to_painting_node.py

--- image_to_painting_node.py
+++ image_to_painting_node.py
@@ -312,7 +312,7 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Painting" :"Image Painting Assitant",
+    "Painting" :"Image Painting Assistant",
     "ProcessInspyrenetRembg" : "Inspyrenet Rembg Assistant",
     
 }
